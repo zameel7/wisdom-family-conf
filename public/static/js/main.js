00b22baa6bda9730a98c85d77e028739ee70c6e1
ZAMEEL HASSAN <zekyazelu7@gmail.com>
2024-01-07 10:53:03 +0530

    Add footer styles to style.css and form checks

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -17,10 +17,57 @@ document.addEventListener("DOMContentLoaded", function () {
         ].filter((feature) => typeof app[feature] === "function");
 
         db = firebase.firestore();
+
     } catch (e) {
         console.error(e);
     }
 });
+
+// Your list of units
+const units = [
+    'MIYAPADAV', 'ARIMALA', 'UPPALA', 'KUNJATHTHUR', 'KADAMBAR',
+    'PAIVELIKA', 'MACHAMPADY', 'HOSANGADI', 'VORKADY', 'BANDIYOD',
+    'MOGRAL', 'ARIKKADY', 'PATLA', 'CHOWKI', 'MOGRAL PUTHUR',
+    'KALLAKKATTA', 'PERIYADKKA', 'SP NAGAR', 'BC ROAD', 'ANANGOOR',
+    'KASARAGOD TOWN', 'THALANGARA', 'CHOORI', 'BOVIKKANAM', 'CHERKALA',
+    'BEVINJE', 'POVVAL', 'MAASTHIKKUND', 'ADOOR', 'ANGADI MUGAR',
+    'NEERCHAL', 'PERLA', 'BADIYADUKKA', 'KOMBANADUKKAM', 'PARAVANADUKKAM',
+    'CHEMANAD', 'UDUMA PADINJAR', 'MELPARAMB', 'KALANAD', 'PALOTH',
+    'KEEZHOOR', 'KUTTIKKOL', 'HOSDURG', 'KOOLIYANGAL', 'PALLIKKARA',
+    'NEELESHWARAM', 'AJANUR', 'KOLAVAYAL', 'KALLOORAVI', 'PARAPPA',
+    'THRIKKARIPPUR', 'ORIMUKK', 'CHERUVATHTHUR', 'PERUMBATTA', 'PADANNA'
+];
+
+// Function to populate the Unit select dropdown
+function populateUnitDropdown() {
+    const unitSelect = document.getElementById('unit');
+    const iunitSelect = document.getElementById('iunit');
+
+    // Clear existing options
+    unitSelect.innerHTML = '';
+
+    // Add a default option
+    const defaultOption = document.createElement('option');
+    defaultOption.value = '';
+    defaultOption.text = 'Select Place';
+    defaultOption.disabled = true;
+    defaultOption.selected = true;
+    unitSelect.appendChild(defaultOption);
+    iunitSelect.appendChild(defaultOption.cloneNode(true));
+
+    // Add options based on the list of units
+    units.forEach((unit) => {
+        const option = document.createElement('option');
+        option.value = unit;
+        option.text = unit;
+        unitSelect.appendChild(option);
+        iunitSelect.appendChild(option.cloneNode(true));
+    });
+}
+
+// Call the function to populate the Unit select dropdown
+populateUnitDropdown();
+
 function selectRegistrationType(registrationType) {
     var registrationForm = document.getElementById("registrationForm");
     var familyFields = document.getElementById("familyFields");
@@ -86,11 +133,27 @@ function submitForm() {
             const age = document.getElementById("age").value;
             const contact = document.getElementById("contact").value;
             const unit = document.getElementById("iunit").value;
-            const zone = document.getElementById("izone").value;
+            const zone = getZoneByUnit(unit);
             const wisdomMember =
                 document.getElementById("iwisdomMember").value === "true";
             const transportMode =
                 document.getElementById("itransportMode").value;
+
+            // Check if a place is selected
+            if (unit === '') {
+                alert('Please select a place.');
+                document.getElementById("main-disp").style.display = "block";
+                document.getElementById("secondary-disp").style.display = "none";
+                return;
+            }
+
+            // Validate mobile number
+            if (!validateMobileNumber(contact)) {
+                alert('Please enter a valid mobile number.');
+                document.getElementById("main-disp").style.display = "block";
+                document.getElementById("secondary-disp").style.display = "none";
+                return;
+            }
 
 
             // Generate a unique 5-digit registration number
@@ -123,19 +186,37 @@ function submitForm() {
                             ]);
                         })
                         .catch((error) => {
+                            console.log(error);
                             showErrorScreen();
                         });
                 })
                 .catch((error) => {
+                    console.log(error);
                     showErrorScreen();
                 });
         } else if (type === "family") {
             const unit = document.getElementById("unit").value;
-            const zone = document.getElementById("zone").value;
+            const zone = getZoneByUnit(unit);
             const transportMode =
                 document.getElementById("transportMode").value;
             const numberOfMembers =
                 parseInt(document.getElementById("numberOfMembers").value);
+
+            // Check if a place is selected
+            if (unit === '') {
+                alert('Please select a place.');
+                document.getElementById("main-disp").style.display = "block";
+                document.getElementById("secondary-disp").style.display = "none";
+                return;
+            }
+
+            // Validate mobile number
+            if (!validateMobileNumber(contact)) {
+                alert('Please enter a valid mobile number.');
+                document.getElementById("main-disp").style.display = "block";
+                document.getElementById("secondary-disp").style.display = "none";
+                return;
+            }
 
             // Add family members to Firestore with the same family registration number
             const familyMembers = [];
@@ -148,44 +229,46 @@ function submitForm() {
                         const memberContact = document.getElementById(`contact${i}`).value;
                         const memberWisdomMember = document.getElementById(`wisdomMember${i}`).value === "true";
 
+                        const regNo = i !== 0 ? `${memberRegistrationNumber}_${i}` : memberRegistrationNumber;
+
                         // Add family member data to Firestore with custom document ID
                         db.collection("registrations")
-                            .doc(`${memberRegistrationNumber}_${i+1}`)
-                            .set({
+                        .doc(`${memberRegistrationNumber}_${i+1}`)
+                        .set({
+                            name: memberName,
+                            age: memberAge,
+                            contact: memberContact,
+                            isFamily: true,
+                            unit: unit,
+                            zone: zone,
+                            transportMode: transportMode,
+                            numberOfMembers: numberOfMembers,
+                            wisdomMember: memberWisdomMember
+                        })
+                        .then(() => {
+                            familyMembers.push({
                                 name: memberName,
-                                age: memberAge,
-                                contact: memberContact,
-                                isFamily: true,
-                                unit: unit,
-                                zone: zone,
-                                transportMode: transportMode,
-                                numberOfMembers: numberOfMembers,
-                                wisdomMember: memberWisdomMember
-                            })
-                            .then(() => {
-                                familyMembers.push({
-                                    name: memberName,
-                                    registrationNumber:
-                                        `${memberRegistrationNumber}_${i+1}`,
-                                });
-                                console.log("Family member added successfully");
-                            })
-                            .catch((error) => {
-                                console.error(
-                                    `Error adding family member document of ${memberRegistrationNumber}_${i+1}: `,
-                                    error
-                                );
-                                showErrorScreen();
-                            }
-                        );
+                                registrationNumber:
+                                    `${memberRegistrationNumber}_${i+1}`,
+                            });
+                            console.log("Family member added successfully");
+                        })
+                        .catch((error) => {
+                            console.error(
+                                `Error adding family member document of ${memberRegistrationNumber}_${i+1}: `,
+                                error
+                            );
+                        })
+                        .finally(() => {
+                            // Show success screen only after all family members have been added
+                            if (familyMembers.length === numberOfMembers)
+                                showSuccessScreen(familyMembers);
+                        });
                     }
-                    showSuccessScreen(familyMembers);
                 })
                 .catch((error) => {
                     console.log(error);
-                    showErrorScreen();
                 });
-            
         }
     } catch (error) {
         console.error(error);
@@ -244,25 +327,26 @@ function showSuccessScreen(registrationDetails) {
     document.getElementById("secondary-disp").style.display = "block";
     
     const successContent = `
-					<ul id="registrationDetailsList"></ul>
-                    <button
-                        type="button"
-                        class="btn btn-primary"
-                        onclick="formReset()"
-                    >
-                        Close
-                    </button>
-				`;
+        <div class="alert alert-success" role="alert">
+            <h4 class="alert-heading font-weight-bold">സമ്മേളന രെജിസ്ട്രേഷൻ പൂർത്തിയായിരിക്കുന്നു!</h4>
+            <ul id="registrationDetailsList" class="list-group"></ul>
+            <p class="mt-2">സമ്മേളന നഗരിയിൽ വച്ച് കാണാം എന്ന പ്രാർത്ഥനയോടെ...</p>
+            <p>സ്നേഹിതർക്കു കൂടി ഈ പരിപാടി പരിചയപ്പെടുത്തിക്കൊടുക്കാൻ മറക്കല്ലേ!</p>
+            <hr>
+            <button type="button" class="btn btn-primary" onclick="">Donate to Da'wa</button>
+            <button type="button" class="btn btn-primary" onclick="formReset()">Register More</button>
+        </div>
+    `;
 
-    document.getElementById("registrationModalLabel").innerText =
-        "Registration Successful";
+    document.getElementById("registrationModalLabel").innerText = "Registration Successful!";
     document.getElementById("secondary-disp").innerHTML = successContent;
 
     const detailsList = document.getElementById("registrationDetailsList");
 
     registrationDetails.forEach((detail) => {
         const listItem = document.createElement("li");
-        listItem.textContent = `${detail.name}: ${detail.registrationNumber}`;
+        listItem.classList.add("list-group-item");
+        listItem.textContent = `${detail.name}: <bold>${detail.registrationNumber}</bold>`;
         detailsList.appendChild(listItem);
     });
 }
@@ -279,7 +363,7 @@ function showErrorScreen() {
                         class="btn btn-primary"
                         onclick="formReset()"
                     >
-                        Close
+                        Retry
                     </button>
 				`;
     document.getElementById("registrationModalLabel").innerText =
@@ -293,4 +377,72 @@ function formReset() {
     document.getElementById("registrationModalLabel").innerText = "Registration Form";
     document.getElementById("main-disp").style.display = "block";
     document.getElementById("secondary-disp").style.display = "none";
+}
+
+function getZoneByUnit(unit) {
+    const zoneMapping = {
+        'MIYAPADAV': 'Manjeshwar',
+        'ARIMALA': 'Manjeshwar',
+        'UPPALA': 'Manjeshwar',
+        'KUNJATHTHUR': 'Manjeshwar',
+        'KADAMBAR': 'Manjeshwar',
+        'PAIVELIKA': 'Manjeshwar',
+        'MACHAMPADY': 'Manjeshwar',
+        'HOSANGADI': 'Manjeshwar',
+        'VORKADY': 'Manjeshwar',
+        'BANDIYOD': 'Manjeshwar',
+        'MOGRAL': 'Kumbala',
+        'ARIKKADY': 'Kumbala',
+        'PATLA': 'Kumbala',
+        'CHOWKI': 'Kumbala',
+        'MOGRAL PUTHUR': 'Kumbala',
+        'KALLAKKATTA': 'Kumbala',
+        'PERIYADKKA': 'Kumbala',
+        'SP NAGAR': 'Kasaragod',
+        'BC ROAD': 'Kasaragod',
+        'ANANGOOR': 'Kasaragod',
+        'KASARAGOD TOWN': 'Kasaragod',
+        'THALANGARA': 'Kasaragod',
+        'CHOORI': 'Kasaragod',
+        'BOVIKKANAM': 'Cherkala',
+        'CHERKALA': 'Cherkala',
+        'BEVINJE': 'Cherkala',
+        'POVVAL': 'Cherkala',
+        'MAASTHIKKUND': 'Cherkala',
+        'ADOOR': 'Cherkala',
+        'ANGADI MUGAR': 'Badiyadukka',
+        'NEERCHAL': 'Badiyadukka',
+        'PERLA': 'Badiyadukka',
+        'BADIYADUKKA': 'Badiyadukka',
+        'KOMBANADUKKAM': 'Uduma',
+        'PARAVANADUKKAM': 'Uduma',
+        'CHEMANAD': 'Uduma',
+        'UDUMA PADINJAR': 'Uduma',
+        'MELPARAMB': 'Uduma',
+        'KALANAD': 'Uduma',
+        'PALOTH': 'Uduma',
+        'KEEZHOOR': 'Uduma',
+        'KUTTIKKOL': 'Uduma',
+        'HOSDURG': 'Kanhangad',
+        'KOOLIYANGAL': 'Kanhangad',
+        'PALLIKKARA': 'Kanhangad',
+        'NEELESHWARAM': 'Kanhangad',
+        'AJANUR': 'Kanhangad',
+        'KOLAVAYAL': 'Kanhangad',
+        'KALLOORAVI': 'Kanhangad',
+        'PARAPPA': 'Kanhangad',
+        'THRIKKARIPPUR': 'Trikkaripur',
+        'ORIMUKK': 'Trikkaripur',
+        'CHERUVATHTHUR': 'Trikkaripur',
+        'PERUMBATTA': 'Trikkaripur',
+        'PADANNA': 'Trikkaripur'
+    };
+
+    return zoneMapping[unit];
+}
+
+function validateMobileNumber(mobileNumber) {
+    // Regular expression for a valid Indian mobile number
+    const mobileNumberRegex = /^[6-9]\d{9}$/;
+    return mobileNumberRegex.test(mobileNumber);
 }
